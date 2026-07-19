712c5bae913c5a9e0b2b9d94053d3866c5c99d71
unknown <pedroalvarenga173@gmail.com>
2026-07-19 18:41:25 -0300

    feat: preparation for the API

--- src/modules/whatsapp/whatsapp.controller.ts
+++ src/modules/whatsapp/whatsapp.controller.ts
@@ -1,13 +1,35 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query, Res, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
 import { WhatsappService } from './whatsapp.service';
 
 @Controller('whatsapp')
 export class WhatsappController {
   constructor(private readonly whatsappService: WhatsappService) {}
 
-  //http://localhost:3000/whatsapp/webhook
-  @Post('webhook')
-  receiveWebhook(@Body() payload: any) {
-    return this.whatsappService.handleIncomingMessage(payload);
+  //Rota pra validar o hook com a meta
+  @Get('webhook')
+  verifyWebhook(@Query() query: any, @Res() res: Response) {
+    const mode = query["hub.mode"];
+    const token = query["hub.verify_token"];
+    const challenge = query["hub.challenge"];
+
+    //N esquece de trocar aqui faz favor
+    const VERIFY_TOKEN = "TokenMeta"; 
+
+    if (mode && token === VERIFY_TOKEN) {
+      console.log("Deu boa");
+      //Retornar o codigo de verificação
+      return res.status(HttpStatus.OK).send(challenge);
+    }
+    
+    return res.status(HttpStatus.FORBIDDEN).send("Token invalido");
+  }
+
+  //Rota pra receber as mensagens
+  @Post("webhook")
+  receiveMessage(@Body() payload: any, @Res() res: Response) {
+    this.whatsappService.handleIncomingMessage(payload);
+    //Responder com 200 apenas
+    return res.status(HttpStatus.OK).send("EVENT_RECEIVED");
   }
 }
--- src/modules/whatsapp/whatsapp.service.ts
+++ src/modules/whatsapp/whatsapp.service.ts
@@ -3,9 +3,26 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class WhatsappService {
   handleIncomingMessage(payload: any) {
-    console.log("Alerta possivel resenha");
-    console.log(JSON.stringify(payload, null, 2));
-    
-    return {status: 'success', message: 'Message received'};
+
+    try {
+      //Vê se é do whatszap
+      if (payload.object === "whatsapp_business_account") {
+        const entry = payload.entry?.[0];
+        const changes = entry?.changes?.[0];
+        const value = changes?.value;
+        const message = value?.messages?.[0];
+        const contact = value?.contacts?.[0];
+
+        //Vê se foi uma mensagem
+        if (message) {
+          console.log("Mensagem:");
+          console.log(`De: ${contact?.profile?.name} (${message.from})`);
+          console.log(`Tipo: ${message.type}`);
+          console.log(`Texto: ${message.text?.body}\n`);
+        }
+      }
+    } catch (error) {
+      console.error("Erro ao processar webhook da Meta:", error);
+    }
   }
 }
