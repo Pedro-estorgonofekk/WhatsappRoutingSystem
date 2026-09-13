28222e7466a96eee27adfef6635bed183892001e
unknown <pedroalvarenga173@gmail.com>
2026-09-13 13:29:27 -0300

    feat: attendant dashboard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +7,10 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello()
   }
+
+  @Get('dashboard')
+  @Redirect('/dashboard/', 302)
+  dashboard() {}
 }
